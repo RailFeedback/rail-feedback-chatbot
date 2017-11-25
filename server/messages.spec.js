a57c9421f991831c224d0d4a9903cb65bb3d6510
RHYS SALDANHA <rhys.saldanha@gmail.com>
2017-11-25 23:49:44 +0100

    Update tests accordingly

--- server/messages.spec.js
+++ server/messages.spec.js
@@ -1,11 +1,21 @@
 import handleMessage from './messages';
 
-test('should return toilet as object in present first noun response', () => {
-  const result = handleMessage("toilet",() => 0);
-  expect(result).to.equal("What is wrong with the toilet?");
+test('should return toilet message object with first string response', () => {
+    const result = handleMessage("toilet", () => 0);
+    expect(result.text).to.equal("What is wrong with the toilet?");
+    expect(result.quick_replies).to.have.deep.members([
+        {title: 'unclean', content_type: 'text', payload: 'UNCLEAN'},
+        {title: 'out of order', content_type: 'text', payload: 'OUT_OF ORDER'},
+        {title: 'busy', content_type: 'text', payload: 'BUSY'}
+    ]);
 });
 
-test('should return toilet as object in present second noun response', () => {
-  const result = handleMessage("toilet",() => 1);
-  expect(result).to.equal("What is up with the toilet?");
+test('should return toilet message object with second string response', () => {
+    const result = handleMessage("toilet", () => 1);
+    expect(result.text).to.equal("What is up with the toilet?");
+    expect(result.quick_replies).to.have.deep.members([
+        {title: 'unclean', content_type: 'text', payload: 'UNCLEAN'},
+        {title: 'out of order', content_type: 'text', payload: 'OUT_OF ORDER'},
+        {title: 'busy', content_type: 'text', payload: 'BUSY'}
+    ]);
 });
